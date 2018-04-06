a8485f5e939c0f9f586bdf7f0dfb9cc54414c6d4
Dan Ove Gudmundsen <danove@kolonial.no>
2018-04-06 12:20:53 +0200

    Going back to rotating file handler

--- termostat.py
+++ termostat.py
@@ -4,16 +4,13 @@ import requests
 import logging
 from logging.handlers import RotatingFileHandler
 
-#logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:Temp is')
-#logger = logging.getLogger('my_logger')
-#handler = RotatingFileHandler('temperature.log', maxBytes=10000, backupCount=10)
-#logger.addHandler(handler)    
-formatter = logging.Formatter('%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:Temp is')
+    
+formatter = logging.Formatter('%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:Temperature is')
 
 #Logging function
 
 def setup_logger(name, log_file, level=logging.INFO):
-    handler = logging.FileHandler(log_file)
+    handler = logging.RotatingFileHandler('temperature.log', maxBytes=10000, backupCount=10)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
