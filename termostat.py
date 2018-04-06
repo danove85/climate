c572002985e58fd908aa96c4a8862423b285efee
Dan Ove Gudmundsen <danove@kolonial.no>
2018-04-06 10:22:38 +0200

    Was calling the wrong logging function

--- termostat.py
+++ termostat.py
@@ -6,7 +6,6 @@ from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(filename='temperature.log', level=logging.INFO, format='%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:Temp is')
 
-logger = logging.getLogger('my_logger')
 handler = RotatingFileHandler('temperature.log', maxBytes=100, backupCount=10)
 logger.addHandler(handler)    
 
@@ -96,7 +95,7 @@ while True:
         #Getting the temperature from the sensor
         read_temp = requests.get('http://10.0.0.2/statusjsn.js?components=18179').json()['sensor_values'][0]['values'][0][0]['v']
         print "Current temperature is %f " % (read_temp)
-        logging.info(read_temp)
+        logger.info(read_temp)
         
         #If temperature is lower than requested temperature
         if read_temp <= set_temp - 2.0:
