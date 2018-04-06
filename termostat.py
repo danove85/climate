0f836d40027978015424e07f1547c699c21f6f41
Dan Ove Gudmundsen <danove@kolonial.no>
2018-04-06 21:28:55 +0200

    Rewrote all loggers to deal with duplicated log entries. Also cleaned up the code

--- termostat.py
+++ termostat.py
@@ -4,22 +4,24 @@ import requests
 import logging
 from logging.handlers import RotatingFileHandler
 
-#logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:Temp is')
-#logger = logging.getLogger('my_logger')
-#handler = RotatingFileHandler('temperature.log', maxBytes=10000, backupCount=10)
-#logger.addHandler(handler)    
-formatter = logging.Formatter('%(asctime)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:')
+
+#Setting the format of log output
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s', datefmt='%d/%m/%Y %H:%M:%S:')
+
 
 #Logging function
 
 def setup_logger(name, log_file, level=logging.INFO):
 
-    handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=10)
-    handler.setFormatter(formatter)
+    
 
     logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=10)
+        handler.setFormatter(formatter)
+        logger.setLevel(level)
+        logger.addHandler(handler)
 
     return logger
 
@@ -27,31 +29,34 @@ def setup_logger(name, log_file, level=logging.INFO):
 
 def cool_engage(client):
     try:
-        
         result = client.read_coils(16)
+        
         if result.bits[0] != True:
             client.write_coil(16, True)
             print "Cooling relay is active"
+        
         client.close()
+    
     except:
         print "Unable to connect to ADAM"
-        adam_logger = setup_logger('adam_except', 'exceptions.log')
-        adam_logger.warning("Unable to connect to ADAM")    
+        adam_logger = setup_logger('adam_exception', 'exceptions.log')
+        adam_logger.error("Unable to connect to ADAM")    
     return
         
 
 def cool_disengage(client):
     try:
-        
         result = client.read_coils(16)
         if result.bits[0] != False:
             client.write_coil(16, False)
             print "Cooling relay is not active"
+        
         client.close()
+    
     except:
         print "Unable to connect to ADAM"
-        adam_logger = setup_logger('adam_except', 'exceptions.log')
-        adam_logger.warning("Unable to connect to ADAM")    
+        adam_logger = setup_logger('adam_exception', 'exceptions.log')
+        adam_logger.error("Unable to connect to ADAM")    
     return
         
 
@@ -61,16 +66,17 @@ def cool_disengage(client):
 
 def heat_engage(client):
     try:
-        
         result = client.read_coils(17)
         if result.bits[0] != True:
             client.write_coil(17, True)
             print "Heating relay is active"
+        
         client.close()
+    
     except:
         print "Unable to connect to ADAM"
-        adam_logger = setup_logger('adam_except', 'exceptions.log')
-        adam_logger.warning("Unable to connect to ADAM")    
+        adam_logger = setup_logger('adam_exception', 'exceptions.log')
+        adam_logger.error("Unable to connect to ADAM")    
     return
         
 
@@ -79,23 +85,26 @@ def heat_engage(client):
 
 def heat_disengage(client):
     try:
-        
         result = client.read_coils(17)
         if result.bits[0] != False:
             client.write_coil(17, False)
             print "Heating relay is not active"
+        
         client.close()
+    
     except:
         print "Unable to connect to ADAM"
-        adam_logger = setup_logger('adam_except', 'exceptions.log')
-        adam_logger.warning("Unable to connect to ADAM")    
+        adam_logger = setup_logger('adam_exception', 'exceptions.log')
+        adam_logger.error("Unable to connect to ADAM")    
     return
         
 
-#Main program
+###########-Main program-#############
+
+# Taking input, making sure it is a float and setting the range of input
 
 z = 1
-# Taking input, making sure it is a float and setting the range of input
+
 while z == True:
     try:
         set_temp = raw_input('Please enter desired temperature. Range is -10 to +30 degrees Celsius:')
@@ -105,26 +114,27 @@ while z == True:
         
         else:
             print "Out of range"
+    
     except ValueError:
         print "Only numbers please."
-        input_logger = setup_logger('incoming_except', 'exceptions.log')
-        input_logger.warning('Input error %s' % set_temp) 
+        input_logger = setup_logger('input_exception', 'exceptions.log')
+        input_logger.warning(set_temp) 
 
 
 #Infinite loop
 
 while True:
+    
     # Defining the ADAM module
     client = ModbusTcpClient('10.0.0.1')
-    
-
     
     try:
         #Getting the temperature from the sensor
         read_temp = requests.get('http://10.0.0.2/statusjsn.js?components=18179').json()['sensor_values'][0]['values'][0][0]['v']
+        
         print "Current temperature is: %f C, Set temperature is: %f C" % (read_temp, set_temp)
-        temp_logger = setup_logger('temp', 'temperature.log')
-        temp_logger.info('Temperature is %f' % read_temp)
+        temp_logger = setup_logger('temperature_is', 'temperature.log')
+        temp_logger.info(read_temp)
         
         #If temperature is lower than requested temperature
         if read_temp <= set_temp - 2.0:
@@ -145,9 +155,11 @@ while True:
     except:
         print "Unable to get temperature from temp sensor"
         get_temp_logger = setup_logger('get_temp_except', 'exceptions.log')
-        get_temp_logger.warning("Unable to get temperature from temp sensor")
+        get_temp_logger.error("Unable to get temperature from temp sensor")
 
     
     time.sleep(5)
+
+
 
 
